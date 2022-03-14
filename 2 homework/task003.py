f8a2b24df885892fc64b3cab1c5aa34f3169b043
mngcndl <sldycpex@gmail.com>
2022-03-14 22:45:26 +0300

    task003 (done)

---
+++ 2 homework/task003.py
@@ -0,0 +1,20 @@
+a = list(input().split())
+
+ans = ''
+opers = ('+', '-', '*', '/')
+vars = []
+
+for i in range(len(a)):
+    if a[i] in opers:
+        var1 = str(vars.pop())
+        if ans == '':
+            var2 = str(vars.pop())
+            ans = var1 + ' ' + a[i] + ' ' + var2
+        else:
+            if a[i] == '+' or a[i] == '-':
+                ans = var1 + ' ' + a[i] + ' ' + '(' + ans + ')'
+            else:
+                ans = '(' + ans + ') ' + a[i] + ' ' + var1
+    else:
+        vars.append(str(a[i]))
+print(ans)
