3054e040478260b00112a03ecf9a0f4721f3006e
mngcndl <sldycpex@gmail.com>
2022-04-06 21:53:28 +0300

    task 012 full

--- 4 homework/task012.py
+++ 4 homework/task012.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+
 def calculate(ar1: int, opr1: str, ar2: int, opr2: str, ar3: int, opr3: str, ar4: int, opr4: str, ar5: int) -> float:
     opers = [opr1, opr2, opr3, opr4]
     args = [ar1, ar2, ar3, ar4, ar5]
@@ -33,8 +36,12 @@ def curried_calculate(ar1: int):
                         def arg4(ar4: int):
                             def oper4(opr4: str):
                                 def arg5(ar5: int):
-                                    print(str(ar1) + opr1 + str(ar2) + opr2 + str(ar3) + opr3 + str(ar4) + opr4 + str(ar5))
-                                    return eval(str(ar1) + opr1 + str(ar2) + opr2 + str(ar3) + opr3 + str(ar4) + opr4 + str(ar5))
+                                    print(str(ar1) + opr1 + str(ar2) + opr2 + str(ar3) + opr3 + str(ar4) + opr4 + str(
+                                        ar5))
+                                    return eval(
+                                        str(ar1) + opr1 + str(ar2) + opr2 + str(ar3) + opr3 + str(ar4) + opr4 + str(
+                                            ar5))
+
                                 return arg5
 
                             return oper4
@@ -54,3 +61,32 @@ def curried_calculate(ar1: int):
 
 print(curried_calculate(1)('+')(1)('*')(2)('-')(3)('/')(10))
 # eval считает ответ по приоритетности операций, а мой метод - нет, поэтому ответы отличаются
+
+
+listed_opers = ['+', '*', '-', '/']
+listed_args = [1, 1, 2, 3, 10]
+
+
+def calculate_for_partial(args, opers) -> float:
+    for i in range(4):
+        if opers[i] == '+':
+            res = args[0] + args[1]
+            args.pop(0)
+            args[0] = res
+        elif opers[i] == '*':
+            res = args[0] * args[1]
+            args.pop(0)
+            args[0] = res
+        elif opers[i] == '/':
+            res = args[0] / args[1]
+            args.pop(0)
+            args[0] = res
+        else:
+            res = args[0] - args[1]
+            args.pop(0)
+            args[0] = res
+    return args.pop()
+
+
+new_calc = partial(calculate_for_partial, args=listed_args)
+print(new_calc(opers=listed_opers))
