7c9a534e5ec9f2718bff18c4398858aef55cbf2f
mngcndl <sldycpex@gmail.com>
2022-04-06 12:55:54 +0300

    finished task 007 (with the last generator)

--- 3 homework/task007.py
+++ 3 homework/task007.py
@@ -3,7 +3,7 @@ def deg_generator(n):
     while True:
         yield tek_value
         tek_value *= n
-        
+
 
 def even_generator(n):
     n = str(n)
@@ -35,31 +35,41 @@ def prime_before_n_generator(n):
                 yield i
 
 
-def prime_digits_of_n_generator(n):
-    n = str(n)
-    for i in range(len(n)):
-        if is_prime(int(n[i])) and int(n) % int(n[i]) == 0:
-            yield n[i]
+def multipliers_of_n_till_9(n):
+    i = 2
+    multipliers = {1}
+    if n != 1:
+        last = 1
+        while i < n // 2 and last <= 9:
+            if n % i == 0:
+                if i < 9:
+                    multipliers.add(i)
+                if n // i < 9:
+                    multipliers.add(n // i)
+                last = i
+            i += 1
+    return multipliers
 
 
-# def nums_before_n_with_prime_digs_from_n_generator(n):
-#     lst = [1]
-#     for i in range(len(str(n))):
-#         if is_prime(str(n[i])):
-#             lst.append(str(n[i]))
-#     if len(lst) > 1:
-#         for i in range(n):
-#             for j in range(len(lst)):
-#                 if i % lst[j] != 0:
-#                     continue
-#             yield i
+def nums_before_n_that_are_prime_and_in_n_generator(n):
+    list_of_digits = list(set(list(str(n))))
+    if list_of_digits.count('0') > 0:
+        list_of_digits.__delitem__('0')
+    for i in range(1, n):
+        flag = True
+        for j in range(len(list_of_digits)):
+            if i % int(list_of_digits[j]) != 0:
+                flag = False
+                break
+        if flag:
+            yield i
 
 
 deg_gen = deg_generator(15)
 for i in range(10):
     print(next(deg_gen))
 
-print('\n' + 'next generator' + '\n')
+print('\n' + 'next generator')
 
 even_gen = even_generator(-9182.10238978768)
 while True:
@@ -69,7 +79,7 @@ while True:
         print('these are all even digits in this number')
         break
 
-print('\n' + 'next generator' + '\n')
+print('\n' + 'next generator')
 
 prime_gen = prime_before_n_generator(103)
 while True:
@@ -77,4 +87,14 @@ while True:
         print(next(prime_gen))
     except StopIteration:
         print('these are all prime numbers between 0 and this number')
+        break
+
+print('\n' + 'next generator')
+
+last_gen = nums_before_n_that_are_prime_and_in_n_generator(185)
+while True:
+    try:
+        print(next(last_gen))
+    except StopIteration:
+        print('these are all prime numbers that you were looking for')
         break
