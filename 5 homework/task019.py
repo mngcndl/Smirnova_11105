42c75b9c9a11d3ea15ba706b1ad95b45b3f2cf51
mngcndl <sldycpex@gmail.com>
2022-05-14 15:43:24 +0300

    task 019 done

---
+++ 5 homework/task019.py
@@ -0,0 +1,19 @@
+file = open(r"Tasks014-020.txt", encoding="utf-8", mode='r')
+line = file.readline()
+
+from task016 import correct_time
+from task017 import abbreviation
+
+i = 1
+while line:
+    string_res016 = correct_time(line)
+    string_res017 = abbreviation(line)
+    if string_res016:
+        print("RegEx \"correct_time\", string number: " + str(i))
+        print(str(string_res016) + '\n')
+    if string_res017:
+        print("RegEx \"abbriveation\", string number: " + str(i))
+        print(str(string_res017) + '\n')
+    line = file.readline()
+    i += 1
+
